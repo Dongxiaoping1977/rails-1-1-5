b0fbccaa5c8c4bef7434299a7532af36581a5770
Xiaoping <13301412096@163.com>
2017-05-04 20:56:37 +0800

    implement my posts

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -3,4 +3,16 @@ class Account::PostsController < ApplicationController
   def index
     @posts = current_user.posts
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:alert] = "Post deleted"
+    redirect_to posts_path
+  end
+  
 end
